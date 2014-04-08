f000c4a95f0b2b892e7bdc207483a7910abd5cac
Riley Shaw <rileyjshaw@gmail.com>
2014-04-08 16:43:50 -0400

    Try to clear the branch

--- app/js/main.jsx
+++
@@ -1,9 +0,0 @@
-/** @jsx React.DOM */
-
-var React = require('react/addons');
-var UI = require('../react/ui.jsx');
-
-React.renderComponent(
-  <UI url="SERVER-URL-HERE" />,
-  document.getElementById('app-container')
-);
