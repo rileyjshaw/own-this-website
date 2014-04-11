4e97c46dbff708a76e39f47e7df5ff7e4875289a
Riley Shaw <rileyjshaw@gmail.com>
2014-04-11 05:00:17 -0400

    Add colon to port

--- app/js/main.jsx
+++ app/js/main.jsx
@@ -4,6 +4,6 @@ var React = require('react/addons');
 var UI = require('../react/ui.jsx');
 
 React.renderComponent(
-  <UI url="http://107.170.85.185" socketPort='8000' />,
+  <UI url="http://107.170.85.185" socketPort=':8000' />,
   document.getElementById('app-container')
 );
