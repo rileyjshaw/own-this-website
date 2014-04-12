f9c670a2af93f90f1c566985facbbd478f57ac97
Riley Shaw <rileyjshaw@gmail.com>
2014-04-11 20:23:03 -0400

    Throttle sockets to stump the setInterval kiddies

--- server/main.js
+++ server/main.js
@@ -3,6 +3,9 @@ var redis = require('redis');
 var redis_client = redis.createClient();
 
 var newConnections = [];
+var ipSpamChecker = {};
+var socketSpamChecker = {};
+
 var king = {
   name: 'NOBODY',
   score: 0
@@ -25,11 +28,13 @@ var officialScoreKeeper = setInterval(function() {
   // Update the king's score locally
   king.score++;
 
+  // Clear the spam checker
+  ipSpamChecker = {};
+  socketSpamChecker = {};
 }, 1000);
 
 // TODO: set up publish / subscribe
 // https://github.com/mranney/node_redis
-
 redis_client.on("error", function (err) {
   console.log("Redis error: " + err);
 });
@@ -50,6 +55,34 @@ function changeStoredKing(name, score) {
 
 function setKing(name, socket) {
   var score;
+  var ipSpamCount = ipSpamChecker[socket.ipAddress];
+  var socketSpamCount = socketSpamChecker[socket.id];
+
+  // Check for spamming from a single socket (warning at > 5 / second)
+  if(!socketSpamCount) {
+    socketSpamChecker[socket.id] = 1;
+  } else if(socketSpamCount > 5) {
+    if(socket.socketWarningFlag) {
+      socket.emit('news', 'There\'s too much traffic from your computer; refresh to reconnect!');
+      socket.disconnect();
+    } else {
+      socket.socketWarningFlag = 1;
+      socket.emit('news', 'It looks like you\'re sending a lot of requests... you aren\'t cheating, are you?');
+    }
+  } else ++socketSpamChecker[socket.id];
+
+  // Check for spamming from a single IP address (warning at > 400 / second)
+  if(!ipSpamCount) {
+    ipSpamChecker[socket.ipAddress] = 1;
+  } else if(++ipSpamCount > 400) {
+    if(socket.ipWarningFlag) {
+      socket.emit('news', 'There\'s too much traffic from your network; refresh to reconnect!');
+      socket.disconnect();
+    } else {
+      socket.ipWarningFlag = 1;
+      socket.emit('news', 'It looks like you\'re sending a lot of requests... you aren\'t cheating, are you?');
+    }
+  } else ++ipSpamChecker[socket.ipAddress];
 
   if(typeof name === 'string') {
     name = name.toUpperCase();
@@ -68,6 +101,7 @@ function setKing(name, socket) {
 }
 
 io.sockets.on('connection', function(socket) {
+  socket.ipAddress = socket.handshake.address.address + ':' + socket.handshake.address.port;
   newConnections.push(socket);
   socket.on('setKing', function(name) {
     setKing(name, socket);
